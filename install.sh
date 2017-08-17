e8ed3e95644d0ed5140558f790b23c7f90bad04a
Johannes Layher <johannes.layher@googlemail.com>
2017-08-17 19:20:09 +0200

    Improve install script to be able to update as well

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Installs gtv in your system for all users. Needs root privileges.
+# Installs gtv in your system for all users or updates the gtv script
+# if applicable. Needs root privileges.
 
 ################################################################################
 
@@ -28,7 +29,58 @@
 
 ################################################################################
 
-cd /usr/local/bin/
-curl -sL $(curl -s https://api.github.com/repos/jola5/gtv/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) --output git-tag-version
-chmod +x git-tag-version
-ln -s git-tag-version gtv
+INSTALL_PATH="/usr/local/bin"
+GTV_RELEASE_URL="https://api.github.com/repos/jola5/gtv/releases"
+VERSION_GREP="([0-9].){2}[0-9]"
+
+# version comparison thanks to https://stackoverflow.com/a/4024263
+function verlte() {
+    [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+function verlt() {
+  [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
+
+function isAlreadyInstalled() {
+  [ -f "${INSTALL_PATH}/git-tag-version" ]
+}
+
+function scriptNeedsUpdating() {
+  latestVersion=$(curl -s ${GTV_RELEASE_URL} | grep tag_name | head -1 | grep -oE ${VERSION_GREP})
+  installedVersion=$(git-tag-version version | grep -oE ${VERSION_GREP})
+  verlt ${installedVersion} ${latestVersion}
+}
+
+function deleteExistingVersion() {
+  rm -rf "${INSTALL_PATH}/gtv" "${INSTALL_PATH}/git-tag-version"
+}
+
+function installLatestVersion() {
+  echo "installing gtv"
+  curl -sL $(curl -s ${GTV_RELEASE_URL} | grep browser_download_url | head -n 1 | cut -d '"' -f 4) --output "${INSTALL_PATH}/git-tag-version"
+  chmod +x "${INSTALL_PATH}/git-tag-version"
+  ln -s "${INSTALL_PATH}/git-tag-version" "${INSTALL_PATH}/gtv"
+}
+
+function updateVersion() {
+  echo "updating gtv"
+  deleteExistingVersion
+  installLatestVersion
+}
+
+function alreadyLatestVersion() {
+  echo "gtv is already the latest version installed"
+}
+
+if ! isAlreadyInstalled; then
+  installLatestVersion
+else
+  if scriptNeedsUpdating; then
+    updateVersion
+  else
+    alreadyLatestVersion
+  fi
+fi
+
+echo "$(gtv version) is available"
